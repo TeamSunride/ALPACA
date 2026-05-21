f0e8cec57184c4710739c12530caa7a5e75f3520
GregKu1 <grigkurnosov@gmail.com>
2026-05-21 19:47:01 +0000

    switches now send messages but there is disconnection error

--- python_scripts/switchSending.py
+++ python_scripts/switchSending.py
@@ -37,31 +37,56 @@ def on_disconnect_callback(client, userdata, rc): # last will
     if rc != 0:
         print("connection lost, closing all valves")
         client.publish(SEQUENCE_TOPIC, json.dumps(payloads["ESTOP"]))
+        print(rc)
 
-def parse_keystroke(keystroke):
-    if keystroke == "abort":
-        return
-    if keystroke == "estop":
-        client.publish(SEQUENCE_TOPIC, json.dumps(payloads["ESTOP"]))
-    if keystroke == "launch":
-        client.publish(SEQUENCE_TOPIC, json.dumps(payloads["LAUNCH_POPPET"])) # CORRESPONDS TO INJECTOR TYPE
-    if keystroke == "fill open":
-        client.publish(SERVO_TOPIC, json.dumps(payloads["FILL_OPEN"]))
-    if keystroke == "fill close":
-        client.publish(SERVO_TOPIC, json.dumps(payloads["FILL_CLOSE"]))
-
+def parse_keystroke(key):
+    match key:
+        case "0": #abort
+            return 
+        case "1": #prop armed
+            return 
+        case "2": #prop disarmed
+            return
+        case "3": #switches armed
+            return
+        case "4": #switches disarmed
+            return
+        case "5": #estop
+            client.publish(SEQUENCE_TOPIC, json.dumps(payloads["ESTOP"]))
+        case "6": #launch
+            client.publish(SEQUENCE_TOPIC, json.dumps(payloads["LAUNCH_POPPET"])) #modify according to injector type
+        case "7": #fill open
+            client.publish(SERVO_TOPIC, json.dumps(payloads["FILL_OPEN"]))
+        case "8": #fill close
+            client.publish(SERVO_TOPIC, json.dumps(payloads["FILL_CLOSE"]))
+        case "9": #fuel open
+            client.publish(SERVO_TOPIC, json.dumps(payloads["FUEL_OPEN"]))
+        case "10": #fuel close
+            client.publish(SERVO_TOPIC, json.dumps(payloads["FUEL_CLOSE"]))
+        case "11": #ox open
+            client.publish(SERVO_TOPIC, json.dumps(payloads["OX_CLOSE"]))  
+        case "12": #ox close
+            client.publish(SERVO_TOPIC, json.dumps(payloads["OX_CLOSE"]))
+        case "13": #dump open
+            client.publish(SERVO_TOPIC, json.dumps(payloads["DUMP_OPEN"]))
+        case "14": #dump close
+            client.publish(SERVO_TOPIC, json.dumps(payloads["DUMP_CLOSE"])) 
+            
+        
 
 client = mqtt.Client()
 client.on_disconnect = on_disconnect_callback
 
-client.connect("localhost", 1883, 10)
+client.connect("localhost", 1883, 15)
 
 ser = serial.Serial(SERIAL_PORT)
 
 while(ser.isOpen):
     keystroke = ser.readline()
-    print(keystroke)
-    parse_keystroke(keystroke)
+    decoded_keystroke = keystroke.decode('ascii').strip()
+    # print(decoded_keystroke)
+    # print(type(decoded_keystroke))
+    parse_keystroke(decoded_keystroke)
 
 
 # while (1):
