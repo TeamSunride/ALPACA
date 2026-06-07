196c47abedcbbf46a616f98f154353949af82d62
GregKu1 <grigkurnosov@gmail.com>
2026-06-07 19:32:12 +0000

    insanity startup scripts entire thing works with one button ive gone maaaad

---
+++ startup_script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#start docker containers
+sudo docker compose -f /home/sunride/ALPACA/docker-compose.yml up -d
+
+#wait for influxdb to be healthy
+echo "waiting for influxdb..."
+until docker exec influxdb curl -sf --max-time 3 http://localhost:8086/health > /dev/null; do
+	sleep 2
+done
+echo "influxdb ready!"
+
+
+#wait for telegraf to be running
+echo "waiting for influxdb..."
+until docker ps --filter "name=telegraf" --filter "status=running" | grep -q telegraf; do
+	sleep 2
+done
+echo "telegraf ready!"
+
+sleep 1
+
+echo "starting scripts"
+xfce4-terminal --hold --command='/bin/bash -c "source /home/sunride/ALPACA/.venv/bin/activate && python3 /home/sunride/ALPACA/python_scripts/switchSending.py"' &
+xfce4-terminal --hold --command='/bin/bash -c "source /home/sunride/ALPACA/.venv/bin/activate && python3 /home/sunride/ALPACA/python_scripts/timestamper.py"' &
+echo "scripts started"
+
+sleep 1
+
+setsid -f chromium --new-instance --new-window 'http://localhost:3001/dashboards' >/dev/null 2>&1 < /dev/null
+
